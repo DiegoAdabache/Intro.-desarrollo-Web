0acec9a4b5c92c1279ca8b6a3c9fd8e776e9ba12
DiegoAdabache <diego.adabache@gmail.com>
2025-09-19 10:01:16 -0600

    Tarea de práctica sobre funciones
    Creamos nuestras primeras funciones en javaScript.

---
+++ EntregaFunciones.js
@@ -0,0 +1,21 @@
+// Multiplicación de dos números
+
+const multiplicar=(a,b) => {
+    return a*b;
+
+}
+
+function multiClasico(a,b) {
+    return a*b;
+}
+
+/// Cuadrado
+
+const cuadrado=(a) => {
+    return a*a;
+}
+
+function cuadradoClasico(a) {
+    return a*a;
+}
+
